36f1a82bcff5f176074f7643d0f8a1d78a112ccf
andy3o0 <mianmuhmmadusman@gmail.com>
2021-11-26 13:58:49 +0500

    button text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -43,7 +43,7 @@ export default function ActionAreaCard() {
               sendData(event, users);
             }}
           >
-            SEND DATA
+            CLICK TO SEND DATA
           </button>
         </CardContent>
       </CardActionArea>
